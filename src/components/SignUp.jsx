c8832743b03394175081d66ff2a9b5869479549d
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-06 15:38:16 -0500

    Create sign up component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,33 @@
+const SignUp = () => {
+    return(
+        <div className="signup_container">
+            <form action="" method="POST" className="signup_form">
+                <div className="form_input_container">
+
+                    <div className="form_group">
+                        <label for='firstName'>First Name: </label>
+                        <input type="text" name='firstName' id="firstName" required></input>
+                    </div>
+
+                    <div className="form_group">
+                        <label for='username'>Username: </label>
+                        <input type="text" name='username' id="username" required></input>
+                    </div>
+
+                    <div className="form_group">
+                        <label for='password'>Password: </label>
+                        <input type="password" name='password' id="password" required></input>
+                    </div>  
+
+                     <div className="form_group">
+                        <label for='confirm_password'>Confirm Password: </label>
+                        <input type="password" name='confirm_password' id="confirm_password" required></input>
+                    </div>
+
+                    <button type="submit">Sign Up</button>                  
+
+                </div>
+            </form>
+        </div>
+    )
+};
