3014ed963182980c18a73216b6e7d3e79f697bb4
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-06 15:46:30 -0500

    Add login and signup routes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
+import Login from "./LogIn";
+import SignUp from "./SignUp";
 
 const Router = () => {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <App />,
-        }
+        },
+        {
+            path: "/login",
+            element: <Login />
+        },
+        {
+            path: "/signup",
+            element: <SignUp />
+        },
     ])
 
     return <RouterProvider router={router} />;
