00fc6db77c16e1a6df544f3bb5aa7364a171c54e
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-06 19:48:23 -0500

    Create PostPreview component

---
+++ src/components/PostPreview.jsx
@@ -0,0 +1,10 @@
+const PostPreview = () => {
+    return(
+        <div className="post_preview">
+            <h3 className="post_title_preview"></h3>
+            <p className="synopsis"></p>
+        </div>
+    )
+};
+
+export default PostPreview
