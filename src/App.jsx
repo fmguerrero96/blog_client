f38fb4aefd1a9347dd1a80e911dc78906e78d594
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-12 16:19:44 -0400

    Implement 'loggedIn' logic and 'user' state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import NavBar from "./components/NavBar"
 import HomePage from "./components/HomePage";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+      const token = localStorage.getItem('token')
+
+      if(token){
+          setLoggedIn(true)
+          const decodedToken = atob(token.split('.')[1])
+          const userInfo = JSON.parse(decodedToken).user
+          setUser(userInfo)
+      } else {
+          setLoggedIn(false)
+          setUser(null)
+      }
+  }, []);
+
+  const handleLogOut = () => {
+      localStorage.removeItem('token')
+      setLoggedIn(false)
+      setUser(undefined)
+  }
 
   return (
    <div>
       <div className="navContainer">
-        <NavBar></NavBar>
+        <NavBar loggedIn={loggedIn} user={user} handleLogOut={handleLogOut}></NavBar>
         <HomePage></HomePage>
       </div>
    </div>
