e8c5d457ea519ec19f22a79baa03c9c599c0faf3
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-19 17:29:12 -0400

    Move state and useEffect logic from App component to Router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
 import NavBar from "./components/NavBar"
 import HomePage from "./components/HomePage";
-import { useState, useEffect } from "react";
 
-function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [user, setUser] = useState(undefined)
-  const [blogPosts, setBlogPosts] =useState([])
-
-  useEffect(() => {
-      const token = localStorage.getItem('token')
-
-      if(token){
-          setLoggedIn(true)
-          const decodedToken = atob(token.split('.')[1])
-          const userInfo = JSON.parse(decodedToken).user
-          setUser(userInfo)
-      } else {
-          setLoggedIn(false)
-          setUser(null)
-      }
-  }, []);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/blog/posts')
-      .then(res => res.json())
-      .then(res => setBlogPosts(res))
-  }, []);
-
-
-  const handleLogOut = () => {
-      localStorage.removeItem('token')
-      setLoggedIn(false)
-      setUser(undefined)
-  }
+function App({ loggedIn, user, handleLogOut, blogPosts }) {
 
   return (
    <div>
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,20 +2,52 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Login from "./LogIn";
 import SignUp from "./SignUp";
+import { useState, useEffect } from "react";
 
 const Router = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [user, setUser] = useState(undefined)
+    const [blogPosts, setBlogPosts] =useState([])
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if(token){
+            setLoggedIn(true)
+            const decodedToken = atob(token.split('.')[1])
+            const userInfo = JSON.parse(decodedToken).user
+            setUser(userInfo)
+        } else {
+            setLoggedIn(false)
+            setUser(null)
+        }
+    },[]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/blog/posts')
+        .then(res => res.json())
+        .then(res => setBlogPosts(res))
+    }, []);
+
+
+    const handleLogOut = () => {
+        localStorage.removeItem('token')
+        setLoggedIn(false)
+        setUser(undefined)
+    }
+
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <App />,
+            element: <App loggedIn={loggedIn} user={user} handleLogOut={handleLogOut} blogPosts={blogPosts} />,
         },
         {
             path: "/login",
-            element: <Login />
+            element: <Login loggedIn={loggedIn} user={user} handleLogOut={handleLogOut}/>
         },
         {
             path: "/signup",
-            element: <SignUp />
+            element: <SignUp loggedIn={loggedIn} user={user} handleLogOut={handleLogOut}/>
         },
     ])
 
