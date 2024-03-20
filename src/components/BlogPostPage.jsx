ab208e7dd98f76cad45b73d303e4cd46dc0299e9
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-20 14:48:17 -0400

    Create a 'post' state variable and fetch the correct blog post based on url 'id' param

--- src/components/BlogPostPage.jsx
+++ src/components/BlogPostPage.jsx
@@ -1,14 +1,25 @@
 import NavBar from "./NavBar"
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const BlogPostPage = ({ loggedIn, user, handleLogOut }) => {
+    const { id } = useParams();
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/blog/posts/${id}`)
+        .then(res => res.json())
+        .then(res => setPost(res))
+    }, []);
+
 
     return (
         <div className="postPage">
             <NavBar loggedIn={loggedIn} user={user} handleLogOut={handleLogOut}></NavBar>
 
             <div className="postContainer">
-                <h1 className="postTitle"></h1>
-                <p className="postText"></p>
+                <h1 className="postTitle">{post.title}</h1>
+                <p className="postText">{post.text}</p>
             </div>
         </div>
     )
