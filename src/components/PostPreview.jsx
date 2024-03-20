1248775d790117a1cf933fc2ea0e96bdc34e040e
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-20 14:43:48 -0400

    Wrap the post's title in a Link

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const PostPreview = ({ post }) => {
     const synopsis = post.text.substring(0, 100)
 
     return(
         <div className="post_preview" key={post._id}>
-            <h3 className="post_title_preview">{post.title}</h3>
+            <Link to={`/blogPost/${post._id}`}>
+                <h3 className="post_title_preview">{post.title}</h3>
+            </Link>
             <p className="synopsis">{synopsis}</p>
         </div>
     )
