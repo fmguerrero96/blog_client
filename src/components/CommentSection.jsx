b2b7b6e4ac126b7c4a921957e83c63842816ae9a
Misael Guerrero <mguerrerowest@gmail.com>
2024-03-24 21:29:24 -0400

    Create handleSubmit function and update formData state variable

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,23 +1,51 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const CommentSection = ({ postId }) => {
+const CommentSection = ({ postID, user }) => {
     const [comments, setComments] = useState([]);
-    const [formData, setFormData] = useState({ message: '' });
+    const [formData, setFormData] = useState({
+        message: '',
+        _id: user._id,
+        postid: postID
+    });
+    const [error, setError] = useState('');
 
     useEffect(() => {
         // Fetch comments from the backend
-        fetch(`http://localhost:3000/blog/posts/${postId}/comments`)
+        fetch(`http://localhost:3000/blog/posts/${postID}/comments`)
             .then(res => res.json())
             .then(data => setComments(data))
             .catch(error => console.error('Error fetching comments:', error));
-    }, [postId]); 
+    }, []); 
 
     const handleChange = (e) => {
         setFormData({ 
             ...formData, [e.target.name]: e.target.value 
         });
     };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:3000/blog/comments/${postID}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            if (!response.ok) {
+                throw new Error('Failed to submit comment');
+            }
+            // Clear form data and fetch comments again
+            setFormData({ ...formData, message: '' });
+            // fetchComments();
+            console.log(formData)
+        } catch (error) {
+            console.error('Error submitting comment:', error.message);
+            setError('Failed to submit comment');
+            console.log(error)
+        }
+    };
+
     
     return(
         <div className="comment_section_container">
